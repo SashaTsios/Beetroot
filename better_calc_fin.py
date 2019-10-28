c4baf0efb2ac75ed882359bc009865c5a0fd4c03
SashaTsios <mr.tsios@gmail.com>
2019-10-28 12:41:59 +0200

    made changes w line length, def size, comments etc

--- better_calc_fin.py
+++ better_calc_fin.py
@@ -1,3 +1,5 @@
+from random import randint
+
 questions = [
     {
         "content": "2 + 2 - 4 = ",
@@ -34,20 +36,19 @@ questions = [
 ]
 
 entry_text = 'Let\'s check your math knowledge: {}'
-from random import randint
 
-def random_n():
-	'''get random question number [index]:
-	'''
-	random_question_number = randint(0, len(questions) - 1)
+def random_n(q_list):
+	"""Get random number from sequence len() diapason."""
+	random_question_number = randint(0, len(q_list) - 1)
 	return random_question_number
 
 def beginning(random_question_number):
-	'''shows question[index] + answers, ask users input and checks if the input is in correct form:
-	'''  
+	"""Shows question[index] + answers, ask users input and checks if the input is in correct form.""" 
 	print(entry_text.format(questions[random_question_number]['content']))  
-	for num_of_choices in range(0, len(questions[random_question_number]['choices'])):
-		list_of_variants = str(num_of_choices + 1) + ' variant: ' + questions[random_question_number]['choices'][num_of_choices]['content']
+	global q_choices
+	q_choices = questions[random_question_number]['choices']
+	for num_of_choices in range(0, len(q_choices)):
+		list_of_variants = str(num_of_choices + 1) + ' variant: ' + q_choices[num_of_choices]['content']
 		print(list_of_variants)
 
 	while True:
@@ -60,17 +61,16 @@ def beginning(random_question_number):
 			print(users_a)
 			break		
 	
-	while users_a not in range(1, len(questions[random_question_number]['choices']) + 1):
+	while users_a not in range(1, len(q_choices) + 1):
 		print('Answer is out of list_of_variants')
 		users_a = int(input('Choose the variant: '))
 	else:
 		users_ans = users_a
 	return users_ans												
 
-def answer_check(random_question_number, users_answer):
-	'''if the answer in NOT correct - asks to try again or to quit. Otherwise - asks to answer another question or quit
-	'''
-	while questions[random_question_number]['choices'][int(users_answer) - 1]['is_correct'] is False:
+def answer_is_wrong(random_question_number, users_answer):
+	"""If the answer is wrong - asks to try again or to quit."""
+	while q_choices[int(users_answer) - 1]['is_correct'] is False:
 		print('Not correct')
 		ask = input('To try this question again press \'y\'. To quit \'q\': ')
 		if ask.lower() == 'y':
@@ -82,45 +82,41 @@ def answer_check(random_question_number, users_answer):
 					print('Please enter a variant correctly!')
 				else:
 					break
-			while users_a not in range(1, len(questions[random_question_number]['choices']) + 1):
+			while users_a not in range(1, len(q_choices) + 1):
 				print('Answer is out of variants, enter again')
 				users_a = int(input('Enter your variant again: '))
 			else:
 				users_answer = users_a	
-			return answer_check(random_question_number, users_answer)
+			return answer_is_wrong(random_question_number, users_answer)
 		elif ask.lower() == 'q':
 			print('Goodbye looser')
 			exit()
-	while questions[random_question_number]['choices'][int(users_answer) - 1]['is_correct'] is True:				    
+
+def answer_is_correct(random_question_number, users_answer):
+	"""If the answer is correct - asks to answer another question or to quit."""
+	while q_choices[int(users_answer) - 1]['is_correct'] is True:				    
 		print('Correct')
 		ask = input('To try another question press \'y\'. To quit press \'q\': ')
-		list_q_y = ['Y', 'y', 'Q', 'q']
-		list_q = ['Q', 'q']
-		list_y = ['Y', 'y']
 		break
-		# print(ask.lower() == 'y')
-	while ask not in list_q_y:	
+	while ask.lower() != 'y' or ask.lower() != 'q':	
 		print('Wrong action, press \'y\' to continue or \'q\' to quit only')
 		ask = input('To try another question press \'y\'. To quit \'q\': ')
-	if ask in list_q:
+	if ask.lower() == 'q':
 		print('Goodbye buddy!')
 		exit()
-	elif ask in list_y:
+	elif ask.lower() == 'y':
 		asks = ask
 	return asks
 			
-def total():
-	'''final function combines all previous functions and runs the calculator.
-	'''
+def main():
+	"""Final function combines all previous functions and runs the calculator."""
 	ask = 'y'
-	list_y = ['Y', 'y']
-	while ask in list_y:
-		random_question_number = random_n()
-		#beginning(random_question_number)
+	while ask.lower() == 'y':
+		random_question_number = random_n(questions)
 		users_answer = beginning(random_question_number)
-		#answer_check(random_question_number, users_answer)
-		ask = answer_check(random_question_number, users_answer)
+		answer_is_wrong(random_question_number, users_answer)
+		ask = answer_is_correct(random_question_number, users_answer)
 	else:
 		exit()
 
-total()
+main()
